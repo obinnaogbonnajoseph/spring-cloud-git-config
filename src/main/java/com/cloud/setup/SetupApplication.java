9d01d8686589e74ba86684f14d061effa9e0b7a2
Obinna Ogbonna <obinnaogbonnajoseph@gmail.com>
2022-09-07 10:05:31 +0100

    remove EP

--- src/main/java/com/cloud/setup/SetupApplication.java
+++ src/main/java/com/cloud/setup/SetupApplication.java
@@ -3,8 +3,6 @@ package com.cloud.setup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableConfigServer
 @SpringBootApplication
@@ -12,16 +10,6 @@ public class SetupApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SetupApplication.class, args);
-	}
-
-}
-
-@RestController
-class MyController {
-
-	@GetMapping("/api/greeting")
-	public String offerGreeting() {
-		return "How are you?";
 	}
 
 }
